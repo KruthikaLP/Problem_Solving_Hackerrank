10f34b9e37a3d0483a70274f8950b60ec8f39ed3
Kruthika LP <141714115+KruthikaLP@users.noreply.github.com>
2024-09-20 00:00:26 +0530

    Create Making Anagrams.java

---
+++ Making Anagrams.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'makingAnagrams' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static int makingAnagrams(String s1, String s2) {
+    // Write your code here
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+    int c[] = new int[26];
+    for(int i=0;i<s1.length();i++){
+        c[s1.charAt(i)-'a']++;
+    }
+    for(int i=0;i<s2.length();i++){
+        c[s2.charAt(i)-'a']--;
+    }
+    int total=0;
+    for(int n : c){
+        total+=Math.abs(n);
+    }
+    return total;
+ 
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s1 = bufferedReader.readLine();
+
+        String s2 = bufferedReader.readLine();
+
+        int result = Result.makingAnagrams(s1, s2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
