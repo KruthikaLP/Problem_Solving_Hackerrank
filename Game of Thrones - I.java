912ab2cb49ee5babd8ff2857903dd1a9f94d9dfc
Kruthika LP <141714115+KruthikaLP@users.noreply.github.com>
2024-09-20 14:16:46 +0530

    Create Game of Thrones - I.java

---
+++ Game of Thrones - I.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'gameOfThrones' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String gameOfThrones(String s) {
+    // Write your code here
+    int c[] = new int[26];
+     for(int i=0;i<s.length();i++){
+        c[s.charAt(i)-'a']++; 
+     }
+     int addc=0;
+     for(int count:c){
+         if(count%2!=0){
+             addc++;
+         }
+     }
+     if(addc>1){
+         return "NO";
+     }
+     else{
+         return "YES";
+     }
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.gameOfThrones(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
